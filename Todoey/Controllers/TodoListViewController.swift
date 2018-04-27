bf0040d2480817e10391fefbb3f66bd20e01aa4d
andrekeuck <andrekeuck@gmail.com>
2018-04-27 14:01:42 -0400

    Convert to MC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"] //create the array to hold the to-do items
+    var itemArray = [Item]() //create the array to hold the to-do items
     
-    let defaults = UserDefaults.standard //create an entry point to the User Defaults iOS backend database which stores persistent data across loading sessions of the app
+    let defaults = UserDefaults.standard //create an entry point to the User Defaults singleton, which stores persistent data across loading sessions of the app
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+
+        let newItem2 = Item()
+        newItem2.title = "Buy waffles."
+        itemArray.append(newItem2)
+
+        let newItem3 = Item()
+        newItem3.title = "Destroy monster."
+        itemArray.append(newItem3)
         
-        
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] { //set an if statement to check/set a local variable "items" equal to the key value pairs for TodoListArray which is stored in the User Defaults database, and cast the values as strings
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] { //set an if statement to check/set a local variable "items" equal to the key value pairs for TodoListArray which is stored in the User Defaults singleton, and cast the values as strings
             itemArray = items //copy the contents of "items" over to the ItemArray array for display on the screen
         }
         
@@ -40,7 +51,11 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) //set the cell equal to the value of "ToDoItemCell", the section of the TodoListViewController where tasks show up
         
-        cell.textLabel?.text = itemArray[indexPath.row] //set the text label of the entry equal to the array value at the index location
+        let item  = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title //set the text label of the entry equal to the array value at the index location
+        
+        cell.accessoryType = item.done ? .checkmark : .none //ternary operator to replace if/else statement; if item.done is TRUE (i.e. done), set accessoryType to checkmark, if item.done is FALSE (i.e. NOT done) set accessoryType to none
         
         return cell //display the cell with the value set above
     }
@@ -50,11 +65,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { //the function that defines what happens to a row that gets selected/tapped by the user
         //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark { //check to see if the selected row has already been selected, i.e. if .accessoryType is assigned to "checkmark"
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none //if .accessoryType HAS been assigned to checkmark, then reset .accessoryType to .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark //if it HAS NOT been selected, select it with a check mark; i.e. if .accessoryType is set to none, then assign it to .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done //reverse the (boolean) value of the .done property for a given item at indexPath.row
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true) //activates the gray highlight when the item is selected, but animates the de-selection immediately so the item is only highlighted during the tap/interaction
         
@@ -69,11 +82,14 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in //set the title/label value for a button on the alert, choose its style, and define the action to take place once that button is tapped/pressed
             
             //what will happen once the user taps the Add Item button on our UIAlert
-            self.itemArray.append(textField.text!) //add the entered text to the item array established at the top of the code
+
+
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem) //add the entered text to the item array established at the top of the code
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray") //save the newly updated itemArray to the User Defaults in a Key-Value Pair format
-            
-            
             
             self.tableView.reloadData() //reload the tableview to display the updated array/data
         }
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andre Keuck on 4/27/18.
+//  Copyright © 2018 Andre Keuck. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
